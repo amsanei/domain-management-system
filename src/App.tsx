483d9c215f231915999c01617a9eb4bbcf310e45
Amir mohammad <am.sanei79@gmail.com>
2025-05-04 03:41:26 +0330

    Fetch data from api and bind it

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,68 @@
-import { Table } from "antd"
+import { Table, Tag } from "antd";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [tableData, setTableData] = useState([]);
+
+  useEffect(() => {
+    const getDomines = async () => {
+      const res = await fetch(
+        "https://6797aa2bc2c861de0c6d964c.mockapi.io/domain"
+      );
+      const data = await res.json();
+
+      const perrtyData = data.map((item : any) => ({
+        "domin-url": item.domain,
+        "active-status": item.isActive ? (
+          <Tag color="success">Active</Tag>
+        ) : (
+          <Tag color="default">Not Active</Tag>
+        ),
+        "verification-status": (
+          <Tag
+            color={
+              item.status === "verified"
+                ? "success"
+                : item.status === "pending"
+                ? "warning"
+                : "error"
+            }
+          >
+            {item.status}
+          </Tag>
+        ),
+      }));
+      setTableData(perrtyData);
+    };
+    getDomines();
+  }, []);
 
   return (
-   <div>
-    <div className="px-8 py-4 mb-8 border-b border-neutral-300 text-xl font-bold">
-      DMS
+    <div>
+      <div className="px-8 py-4 mb-8 border-b border-neutral-300 text-xl font-bold">
+        DMS
+      </div>
+      <Table dataSource={tableData} columns={columns} />
     </div>
-   <Table dataSource={dataSource} columns={columns} />
-   </div>
-  )
+  );
 }
-
-const dataSource = [
-  {
-    key: '1',
-    name: 'Mike',
-    age: 32,
-    address: '10 Downing Street',
-  },
-  {
-    key: '2',
-    name: 'John',
-    age: 42,
-    address: '10 Downing Street',
-  },
-];
-
 
 const columns = [
   {
-    title: 'Name',
-    dataIndex: 'name',
-    key: 'name',
+    title: "Domin URL",
+    dataIndex: "domin-url",
+    key: "domin-url",
   },
   {
-    title: 'Age',
-    dataIndex: 'age',
-    key: 'age',
+    title: "Active Status",
+    dataIndex: "active-status",
+    key: "active-status",
   },
   {
-    title: 'Address',
-    dataIndex: 'address',
-    key: 'address',
+    title: "Verification Status",
+    dataIndex: "verification-status",
+    key: "verification-status",
   },
 ];
 
-export default App
+export default App;
